865b518e435e3ad823baab237e0f2f5d37104dfa
laflame102 <109043511+laflame102@users.noreply.github.com>
2022-12-24 12:52:12 +0000

    Deploying to gh-pages from @ laflame102/goit-js-hw-09@390c7124a64c9b2d2362393a2b09732dc5ae88b0 🚀

---
+++ 01-color-switcher.37f0c679.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")},e=null;t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!0),e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stop.addEventListener("click",(function(){t.start.removeAttribute("disabled"),clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.37f0c679.js.map
